a37e88133af4c8bb4776c3a3739d12e302214a52
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-29 00:53:55 +0100

    Dynamically added data to google charts.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var security_data;
+
 $(document).ready(function(){
     $('#load-btn').click(function(){
         $.ajax({
@@ -5,7 +7,7 @@ $(document).ready(function(){
             dataType: "text",
             success: function(data)
             {
-                var security_data = data.split(/\r?\n|\r/);
+                security_data = data.split(/\r?\n|\r/);
                 var table_data = '<table>';
                 for(var count=0; count < security_data.length; count++)
                 {
@@ -30,7 +32,56 @@ $(document).ready(function(){
         $('#load-btn').hide();
     })
 
-    $(document).on('click', '#data-table th', function(){
+    function getCounts(colNum){
+        var colNum = colNum-1;
+        var counts = {};
+        for(var count=0; count < security_data.length; count++)
+        {
+            var cell_data = security_data[count].split(",");
+            counts[cell_data[colNum]] =  (counts[cell_data[colNum]] || 0)+ 1;
+        }
+        return counts;
+    }
 
+    $(document).on('click', '#data-table th', function(){
+        var colNum = $(this).text();
+        var counts = getCounts(colNum);
+        console.log(counts);
+        drawChart(colNum);
     })
+
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', {'packages':['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    //google.charts.setOnLoadCallback(drawChart);
+
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart(colNum) {
+
+        // Create the data table.
+        var counts = getCounts(colNum);
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Column');
+        data.addColumn('number', 'Frequency');
+
+        $.each(counts, function(index, value){
+            data.addRow([index, value]);
+        })
+
+        // Set chart options
+        var options = {
+            'title':'Column '+colNum+' Frequencies',
+            'width':500,
+            'height':400
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('graphs'));
+        chart.draw(data, options);
+    }
+
 })
